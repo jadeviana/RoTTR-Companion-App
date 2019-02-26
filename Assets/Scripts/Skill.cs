1e5e889802cadc08346a39e1d6e202e75c19e7d3
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-02-25 22:31:29 -0300

    Skill button prefab and scripts added

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum skillState {locked = 1, available = 2, upgraded = 3};
+[CreateAssetMenu(fileName = "Skill", menuName = "Skill")]
+public class Skill : ScriptableObject {
+	public Sprite skillIcon;
+	public string skillName;
+	public string skillDescription;
+	public int skillCost = 1;
+
+	//Skills requirement
+	public string requirement;
+	public skillState status;
+
+}
---
+++ Assets/Scripts/SkillButtonDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButtonDisplay : MonoBehaviour {
+
+		public Skill item;
+		public Image Lock;
+		public Image Check;
+		public Image skillIcon;
+		public Image background;
+		public Image div;
+		public Image lockedBar;
+
+		private int buttonState;
+	// Use this for initialization
+	void Start () {
+
+		skillIcon.sprite = item.skillIcon;
+		buttonState = (int)item.status;
+		
+		//if the equipment state is locked, it should change the button's colour (red) and set the lock and lock bar image to active
+		if(buttonState == 1){
+			div.color = new Color32(123,6,27,255);
+			background.color = new Color32(123,6,27,255);
+			skillIcon.color = new Color32(123,6,27,255);
+			Lock.gameObject.SetActive(true);
+			lockedBar.gameObject.SetActive(true);
+		}
+
+		//if the equipment state is upgrade, it should change the button's colour (grey) and set the check image to active
+		else if (buttonState == 3)
+		{
+			div.color = new Color32(101,101,101,255);
+			background.color = new Color32(101,101,101,255);
+			skillIcon.color = new Color32(101,101,101,255);
+			Check.gameObject.SetActive(true);
+		}
+	}
+
+}

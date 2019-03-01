c9aa4a7f855b72b90d2c85d282cfccc0d44c17a3
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-02-28 23:09:00 -0300

    Skill screen prefab and script added

---
+++ Assets/Scripts/Skills/SkillScreenDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class SkillScreenDisplay : MonoBehaviour {
+	public Skill ability;
+	public Image skillIcon;
+	public Image Lock;
+	public TextMeshProUGUI barTitle;
+	public TextMeshProUGUI skillTitle;
+	public TextMeshProUGUI description;
+	public TextMeshProUGUI requirement; 
+	public TextMeshProUGUI cost;
+	public TextMeshProUGUI skillPoint;
+
+	// Use this for initialization
+	void Start () {
+		skillIcon.sprite = ability.skillIcon;
+		skillTitle.text = ability.skillName;
+		description.text = ability.skillDescription;
+		requirement.text = ability.requirement;
+	}
+}

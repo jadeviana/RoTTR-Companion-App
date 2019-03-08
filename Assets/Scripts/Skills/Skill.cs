dc986ec11dd5f6f9dbcbda85c212133102327d2f
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-03-08 15:11:46 -0300

    Brawler skills (level 1) added

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum skillState {locked = 1, available = 2, upgraded = 3};
+public enum skillType {brawler = 1, hunter = 2, survivor = 3};
+public enum skillLevel {firstTier = 1, secondTier = 2, thirdTier = 3};
+
 [CreateAssetMenu(fileName = "Skill", menuName = "Skill")]
 public class Skill : ScriptableObject {
 	public Sprite skillIcon;
 	public string skillName;
 	public string skillDescription;
-	public int skillCost = 1;
 
+	public skillState status;
+	public skillType type;
+	public skillLevel level;
+
+	
 	//Skills requirement
 	public string requirement;
-	public skillState status;
-
+	public int skillCost = 1;
 }

bf506dc359038704370c742a1b6c3a181a2e53e1
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-04-20 22:52:24 -0300

    Player stats script added

---
+++ Assets/Scripts/Player stats/PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	public int playerCredits;
+	public int SkillPoints;
+	public int currentCredits{get; private set;}
+	public int currentSkillPoints{get; private set;}
+
+}

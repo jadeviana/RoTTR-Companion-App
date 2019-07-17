cea006b511449a721a4cf71a461673bbf47f17be
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-07-16 21:52:20 -0300

    Screens with each weapon list by type added

--- Assets/Scripts/Weapons/WeaponTypeScreenDisplay.cs
+++ Assets/Scripts/Weapons/WeaponTypeScreenDisplay.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class WeaponTypeScreenDisplay : MonoBehaviour {
 
+	public WeaponType weapon;
+	public TextMeshProUGUI barTitle;
+	public TextMeshProUGUI weaponAvailability;
+
 	// Use this for initialization
 	void Start () {
+
+		barTitle.text = weapon.weaponTypeName;
 		
+		switch((int)weapon.textDisplayed){
+			case 1:
+				weaponAvailability.text = weapon.weaponTypeFound.ToString() + "/" + weapon.weaponTypeTotal.ToString() + " found";
+			break;
+
+			case 2:
+				weaponAvailability.text = weapon.weaponTypeFound.ToString() + "/" + weapon.weaponTypeTotal.ToString() + " available";
+			break;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }

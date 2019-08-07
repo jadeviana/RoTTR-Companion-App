c238a63b5185ea1e197945beb1473fd8200d3cc9
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-08-06 23:30:36 -0300

    Survival guide screen added with Gear and Crafting categories

---
+++ Assets/Scripts/Survival guide/SurvivalGuideItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Survival Guide Item", menuName = "Survival Guide Item")]
+public class SurvivalGuideItem : ScriptableObject {
+	public string ItemName;
+	public string description;
+	
+}

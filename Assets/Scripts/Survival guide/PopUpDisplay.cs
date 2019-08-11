8d30ba000ed396b3dae5564e583bcb078458362e
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-08-11 12:17:28 -0300

    Survival guide content pop-up added

---
+++ Assets/Scripts/Survival guide/PopUpDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopUpDisplay : MonoBehaviour {
+
+	public SurvivalGuideItem listItem;
+	public TextMeshProUGUI itemTitle;
+	public TextMeshProUGUI itemDescription;
+
+	// Use this for initialization
+	void Start () {
+		itemTitle.text = listItem.ItemName;
+		itemDescription.text = listItem.description;
+	}
+
+}

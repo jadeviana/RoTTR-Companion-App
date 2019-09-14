7ee1bc8fc1acf34244f6be9d1fddf771073c4c69
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-09-13 21:25:32 -0300

    Fixed tab prefab in the survival guide

---
+++ Assets/Scripts/Survival guide/TabButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TabButton : MonoBehaviour {
+	[SerializeField] private Image ChevronUp;
+	[SerializeField] private Image ChevronDown;
+	[SerializeField] private GameObject SectionContent;
+	
+	private bool isExpanded;
+	public void ToggleTab () {
+		isExpanded = !isExpanded;
+
+		if(isExpanded == true){
+			ChevronUp.gameObject.SetActive(true);
+			ChevronDown.gameObject.SetActive(false);
+			SectionContent.gameObject.SetActive(true);
+		}
+		else {
+			ChevronUp.gameObject.SetActive(false);
+			ChevronDown.gameObject.SetActive(true);
+			SectionContent.gameObject.SetActive(false);
+		}
+	}
+}

327aa214388030aa613f8a7a1a4fe8dd1605e66e
Jade Viana <45906157+jadeviana@users.noreply.github.com>
2019-09-21 19:52:03 -0300

    Menu added to UIManager object

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+	private GameObject lastScreen;
+	[SerializeField] private GameObject MenuObject;
+	[SerializeField] private GameObject BackgroundObject;
+
+	[Space(18)]
+
+	[SerializeField] private GameObject BaseCampObject;
+	[SerializeField] private GameObject MarketplaceObject;
+	[SerializeField] private GameObject LeaderboardObject;
+	[SerializeField] private GameObject AchievementsObject;
+	[SerializeField] private GameObject SurvivalGuideObject;
+
+	
+	void Update () {
+		if (BaseCampObject.activeSelf)
+		{
+			lastScreen = BaseCampObject;
+		}
+		if (MarketplaceObject.activeSelf)
+		{
+			lastScreen = MarketplaceObject;
+		}
+		if (LeaderboardObject.activeSelf)
+		{
+			lastScreen = LeaderboardObject;
+		}
+		if (AchievementsObject.activeSelf)
+		{
+			lastScreen = AchievementsObject;
+		}
+		if (SurvivalGuideObject.activeSelf)
+		{
+			lastScreen = SurvivalGuideObject;
+		}
+	}
+
+	public void CloseMenu(){
+		if (lastScreen != null)
+		{
+		MenuObject.gameObject.SetActive(false);
+		BackgroundObject.gameObject.SetActive(false);
+		lastScreen.gameObject.SetActive(true);
+		}
+	}
+}
